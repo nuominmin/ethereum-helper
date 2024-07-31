1854ccd0da68db01a980741cce819dbdb26cf7f3
nuominmin <776191753@qq.com>
2024-07-31 14:30:52 +0800

    feat: 添加区块范围验证和处理逻辑
    - 实现 `BlockRangeProcessor` 以管理和处理区块范围。
    - 添加 `Range` 结构体以定义具有相关数据的区块范围。
    - 使用 `ErrInvalidStartBlock` 确保 `StartBlock` 大于 0。
    - 使用 `ErrInvalidRange` 确保 `EndBlock` 大于或等于 `StartBlock`。
    - 使用 `ErrOverlappingRanges` 确保没有重叠的范围。
    - 提供 `Handle` 方法在指定区块范围内处理数据。
    - 根据 `StartBlock` 排序范围以保持顺序。
    - 包含所有验证和处理逻辑的测试。

---
+++ blockranges/processor.go
@@ -0,0 +1,53 @@
+package blockranges
+
+import (
+	"errors"
+	"sort"
+)
+
+// Range 范围
+type Range[T any] struct {
+	StartBlock uint64 // 包含
+	data       T
+}
+
+// ErrOverlappingRanges 用于表示区间重叠错误
+var ErrOverlappingRanges = errors.New("config have overlapping ranges")
+
+// ErrInvalidStartBlock 用于表示起始块无效错误
+var ErrInvalidStartBlock = errors.New("start block must be greater than 0")
+
+type BlockRangeProcessor[T any] struct {
+	ranges []Range[T]
+}
+
+func NewBlockRangeProcessor[T any](ranges ...Range[T]) (*BlockRangeProcessor[T], error) {
+	sort.Slice(ranges, func(i, j int) bool {
+		// 根据 StartBlock 降序
+		return ranges[i].StartBlock > ranges[j].StartBlock
+	})
+
+	for i := 0; i < len(ranges); i++ {
+		if ranges[i].StartBlock == 0 {
+			return nil, ErrInvalidStartBlock
+		}
+		if i > 0 && ranges[i].StartBlock == ranges[i-1].StartBlock {
+			return nil, ErrOverlappingRanges
+		}
+	}
+
+	return &BlockRangeProcessor[T]{
+		ranges: ranges,
+	}, nil
+}
+
+func (pb *BlockRangeProcessor[T]) Handle(blockNumber uint64, handleFn func(data T) error) error {
+	for i := 0; i < len(pb.ranges); i++ {
+		if blockNumber >= pb.ranges[i].StartBlock {
+			return handleFn(pb.ranges[i].data)
+		}
+	}
+
+	var data T
+	return handleFn(data)
+}
